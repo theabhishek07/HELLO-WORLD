e227a8768f0bf2a423551358b3beb1959ec6aa5a
theabhishek07 <asharma0269@gmail.com>
2020-05-10 20:45:07 +0530

    C-Sharp

---
+++ C-Sharp/HelloWorld.cs
@@ -0,0 +1,13 @@
+using System;
+namespace Abhishek
+{
+class HelloWorld
+{
+    static void Main() 
+    {
+        /* No need to specify cmd line argument for Main() */
+        Console.WriteLine("Hello World!");
+        Console.ReadKey();
+    }
+}
+}
